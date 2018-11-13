70a1c06ff49d042c2c2666b07887cf0492e541b2
behouba <behouba@gmail.com>
2018-11-13 22:04:20 +0300

    merge sort implemented

--- sorting/merge-sort.go
+++ sorting/merge-sort.go
@@ -1,34 +1,46 @@
 package sorting
 
 import (
-	"fmt"
 	"math"
 )
 
-func Merge(arr []int) (outputArr []int) {
+// Merge function merge two sorted part of an array and merge them
+func Merge(arr []int) []int {
 	var leftArr, rigthArr []int
 	mid := len(arr) / 2
 	leftArr = append(leftArr, arr[:mid]...)
 	rigthArr = append(rigthArr, arr[mid:]...)
 	leftArr = append(leftArr, math.MaxInt32)
 	rigthArr = append(rigthArr, math.MaxInt32)
-	fmt.Println("left = ", leftArr, "rigth = ", rigthArr, "len = ", len(arr))
 
 	Li, Ri := 0, 0
 
 	for i := 0; i < len(arr); i++ {
 		if leftArr[Li] >= rigthArr[Ri] {
-			outputArr = append(outputArr, rigthArr[Ri])
+			arr[i] = rigthArr[Ri]
 			Ri++
 		} else {
-			outputArr = append(outputArr, leftArr[Li])
+			arr[i] = leftArr[Li]
 			Li++
 		}
 	}
-
-	return
+	return arr
 }
 
+// MergeSort function through recursive
+// call and merge with Merge function to sort an array
+
 func MergeSort(arr []int) []int {
+
+	// save the length off the array
+	arrLength := len(arr)
+
+	// call MergeSort until we get only two element in the array
+	if arrLength > 1 {
+		mid := arrLength / 2
+		MergeSort(arr[:mid])
+		MergeSort(arr[mid:])
+		Merge(arr)
+	}
 	return arr
 }
