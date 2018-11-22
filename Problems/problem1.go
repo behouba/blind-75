acae8a23fefc5818004406463f45284f8fda1068
behouba <behouba@gmail.com>
2018-11-22 22:11:47 +0300

    migratory bird challenge from hackerrank solved

--- Problems/problem1.go
+++ Problems/problem1.go
@@ -15,13 +15,13 @@ For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 Bonus: Can you do this in one pass?*/
 
 // CheckAddUp is my first solution to this problem
+
 func CheckAddUp(arr []int, numb int) bool {
 	for i := 0; i < len(arr); i++ {
 		k := i + 1
 		for j := k; j < len(arr); j++ {
-			fmt.Println(arr[i], " ", arr[j])
 			if arr[i]+arr[j] == numb {
-				fmt.Println("numb are =", arr[i], " and ", arr[j])
+				fmt.Println("numbers are =", arr[i], " and ", arr[j])
 				return true
 			}
 		}
